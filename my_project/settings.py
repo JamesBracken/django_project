7cb5847e537b5b1b42e27c03a03fc7a3c285eb82
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-03-11 20:36:47 +0000

    Fix: index link

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-iq#afk+qt4j^u_+qtodvwqkh4^%b$g&)asv#bpuw+p!&f60$=5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['herokuapp.com',
-                 '127.0.0.1']
+ALLOWED_HOSTS = ['herokuapp.com', '127.0.0.1']
 
 
 # Application definition
